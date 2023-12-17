8e24dc99642f72ed3694a9882890cbb5771e9c10
TheFoxfairy <1479852727@qq.com>
2023-12-17 13:13:51 +0800

    feat：动态注册路由

---
+++ src/main/java/org/foxfairy/base/api/core/route/DynamicUrlRegistry.java
@@ -0,0 +1,86 @@
+package org.foxfairy.base.api.core.route;
+
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class DynamicUrlRegistry {
+
+    @Resource
+    private RequestMappingHandlerMapping mapping;
+    @Resource
+    private RequestHandler handler;
+    private final Map<String, RequestMeta> dynamicMappings = new ConcurrentHashMap<>();
+
+    @SneakyThrows
+    public void registerDynamicUrl(String url) {
+        RequestMappingInfo info = RequestMappingInfo
+                    .paths(url)
+                    .methods(RequestMethod.GET)
+                    .build();
+
+        Method method =  RequestHandler.class.getDeclaredMethod(
+                "invoke",
+                HttpServletRequest.class,
+                HttpServletResponse.class,
+                Map.class, Map.class, Map.class);
+
+        // 检查是否已存在相同映射
+        HandlerMethod handlerMethod = mapping.getHandlerMethods().get(info);
+        if (Objects.isNull(handlerMethod)) {
+            // 不存在，进行注册
+            mapping.registerMapping(info, handler, method);
+        }
+        // 更新
+        dynamicMappings.put(url, new RequestMeta(url, info));
+    }
+
+    @SneakyThrows
+    public void reset() {
+        // 清除之前设置的所有动态路由
+        for (String url : dynamicMappings.keySet()) {
+            mapping.unregisterMapping(dynamicMappings.get(url).getInfo());
+        }
+
+        // 重新加载新的动态路由
+        // 这里可以根据实际需求重新加载新的路由信息
+        for (String url : dynamicMappings.keySet()) {
+            this.registerDynamicUrl(url);
+        }
+
+        Thread.sleep(0);
+    }
+
+    public void removeDynamicUrl(String url){
+        if (Objects.nonNull(dynamicMappings.get(url) )) {
+            mapping.unregisterMapping(dynamicMappings.get(url).getInfo());
+            dynamicMappings.remove(url);
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class RequestMeta{
+        private String url;
+        private RequestMappingInfo info;
+    }
+}
+
---
+++ src/main/java/org/foxfairy/base/api/core/route/RequestHandler.java
@@ -0,0 +1,83 @@
+package org.foxfairy.base.api.core.route;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import com.alibaba.fastjson.JSON;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * 请求入口处理
+ */
+@Slf4j
+@Component
+public class RequestHandler {
+
+    /**
+     * 实际请求入口
+     *
+     * @param request       HttpServletRequest
+     * @param response      HttpServletResponse
+     * @param pathVariables 路径变量
+     * @param parameters    表单参数&URL参数
+     * @return 返回请求结果
+     */
+    @ResponseBody
+    public Object invoke(HttpServletRequest request, HttpServletResponse response,
+                         @PathVariable(required = false) Map<String, Object> pathVariables,
+                         @RequestHeader(required = false) Map<String, Object> defaultHeaders,
+                         @RequestParam(required = false) Map<String, Object> parameters) {
+        
+        // 获取 Cookies
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            String cookiesJson = JSON.toJSONString(cookies);
+            log.info("Cookies: " + cookiesJson);
+        }
+
+        HttpSession session = request.getSession();
+        // 获取 Session 属性值
+        Enumeration<String> attributeNames = session.getAttributeNames();
+        while (attributeNames.hasMoreElements()) {
+            String attributeName = attributeNames.nextElement();
+            Object attributeValue = session.getAttribute(attributeName);
+            log.info("Session Attribute - Name: " + attributeName + ", Value: " + attributeValue);
+        }
+
+        String requestBody = this.getRequestBody(request);
+
+        return "香菜";
+    }
+
+    /**
+     * 获取request body
+     * @param request
+     * @return
+     */
+    private String getRequestBody(HttpServletRequest request) {
+        String requestBody = null;
+        // 获取请求体
+        try (InputStream inputStream = request.getInputStream()) {
+            requestBody = new BufferedReader(new InputStreamReader(inputStream))
+                    .lines()
+                    .collect(Collectors.joining(System.lineSeparator()));
+            log.info("Request Body: " + requestBody);
+
+            return requestBody;
+        } catch (IOException e) {
+            // 处理 IO 异常E
+            log.error("request body获取异常：{}", e.getMessage());
+        }
+        return requestBody;
+    }
+}
