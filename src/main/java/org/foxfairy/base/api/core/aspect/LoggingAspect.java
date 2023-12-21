a5f6b4f3cf02a9119b0a73f7f7f226c6fa44c054
TheFoxfairy <1479852727@qq.com>
2023-12-21 16:10:22 +0800

    styles：调整格式

--- src/main/java/org/foxfairy/base/api/core/aspect/LoggingAspect.java
+++ src/main/java/org/foxfairy/base/api/core/aspect/LoggingAspect.java
@@ -19,14 +19,13 @@ public class LoggingAspect {
 
     @Around("@annotation(loggable)")
     public Object logAroundMethodExecution(ProceedingJoinPoint joinPoint, Loggable loggable) throws Throwable {
-        Object result;
         // 方法名
         String methodName = joinPoint.getSignature().getName();
         //获取动态参数
         String params = spelExpressionResolver.generateKeyBySpEL(loggable.value(), joinPoint);
         log.info("获取动态aop参数: {}", params);
         log.info("执行目标方法: {} ==>>开始......", methodName);
-        result = joinPoint.proceed();
+        Object result = joinPoint.proceed();
         log.info("执行目标方法: {} ==>>结束......", methodName);
         // 返回通知
         log.info("目标方法 " + methodName + " 执行结果 " + result);
